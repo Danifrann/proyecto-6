93f27498495d98906d3b316bb9aa8fa0ae65f4a1
Daniela <danifonsecapesce@gmail.com>
2024-08-14 22:45:33 -0400

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-console.log("process.env.MONGODB_URI");
-console.log(process.env.MONGODB_URI);
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
